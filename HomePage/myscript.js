69abbf644f2ac33e84c14c6682522a9c75a04904
Mukund604 <73462926+Mukund604@users.noreply.github.com>
2022-03-08 22:25:40 +0530

    HomePage Alomst Done

--- HomePage/myscript.js
+++ HomePage/myscript.js
@@ -1,8 +1,13 @@
-$(document).ready(function() {
-    $('#login').click(function() {
-        $('.login_form').addClass('popup');
-    });
-    $('.login_form form .fa-times').click(function() {
-        $('.login_form').removeClass('popup');
-    });
-});
+"use strict";
+const login = document.querySelector(".LogForm");
+const closeButton = document.querySelector(".cross");
+console.log(closeButton);
+const loginImage = document.querySelector("#login");
+const openForm = function () {
+  login.classList.remove("Hidden");
+};
+const closeForm = function () {
+  login.classList.add("Hidden");
+};
+loginImage.addEventListener("click", openForm);
+closeButton.addEventListener("click", closeForm);
